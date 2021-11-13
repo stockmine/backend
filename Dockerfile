069c78fbdf3d95c5948c6c6f2f562623dc8bd028
Antonije Ivanovic <aivanovic@squarespace.com>
2021-11-13 14:41:42 +0000

    Fix docker build.
    Added reddit oAuth.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM adoptopenjdk/openjdk11-openj9
-ADD ./build/libs/web /opt/web
+ADD web /opt/web
 CMD ["/opt/web"]
--- src/main/kotlin/com/github/anivanovic/stockmine/Server.kt
+++ src/main/kotlin/com/github/anivanovic/stockmine/Server.kt
@@ -1,20 +1,29 @@
 package com.github.anivanovic.stockmine
 
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.netty.*
-import java.util.*
+import io.ktor.util.*
+import java.util.UUID
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-var REDDIT_AUTH = "https://www.reddit.com/api/v1/access_token"
+const val REDDIT_AUTORIZE = "https://www.reddit.com/api/v1/authorize"
+const val REDDIT_AUTH = "https://www.reddit.com/api/v1/access_token"
 val client = HttpClient(CIO)
+
+private val redirect_uri = "https://backend-lf3ty7mjqq-ew.a.run.app/reddit"
+
+private val clientId = "tiL3UaYvQqnzkiex7pRciQ"
 
 @JvmOverloads
 fun Application.module(testing: Boolean = false, development: Boolean = false) {
@@ -26,19 +35,27 @@ fun Application.module(testing: Boolean = false, development: Boolean = false) {
             resources("files")
         }
 
+        val ub = URLBuilder(REDDIT_AUTORIZE)
+        ub.parameters.append("client_id", clientId)
+        ub.parameters.append("response_type", "code")
+        ub.parameters.append("state", UUID.randomUUID().toString())
+        ub.parameters.append("redirect_uri", redirect_uri)
+        ub.parameters.append("duration", "permanent")
+        ub.parameters.append("scope", "read")
         get("/reddit-auth") {
-            client.request(REDDIT_AUTH) {
-                method = HttpMethod.Get
-                parameter("client_id", "tiL3UaYvQqnzkiex7pRciQ")
-                parameter("response_type", "code")
-                parameter("state", UUID.randomUUID())
-                parameter("redirect_uri", "https://backend-lf3ty7mjqq-ew.a.run.app/reddit")
-                parameter("duration", "permanent")
-                parameter("scope", "read")
-            }
+            call.respondRedirect(ub.buildString(), false)
         }
         get("/reddit") {
-            call.respond("got reddit code " + call.request.queryParameters["code"])
+            val cred = UserPasswordCredential(clientId, System.getenv("REDDIT_SECRET"))
+            val res: HttpResponse = client.post(REDDIT_AUTH) {
+                parameter("grant_type", "authorization_code")
+                parameter("code", call.request.queryParameters["code"])
+                parameter("redirect_uri", redirect_uri)
+                log.info("Cred: $cred")
+                headers.append("Authorization", "Basic $cred")
+            }
+            val body: String = res.receive()
+            call.respondText(body, ContentType.Application.Json)
         }
     }
 }
