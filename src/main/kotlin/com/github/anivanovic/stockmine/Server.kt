97969dad4275f4d33fb8781b84bd57133dd08203
Antonije Ivanovic <aivanovic@squarespace.com>
2021-11-14 01:00:24 +0000

    Fix running gradle docker commands.

--- src/main/kotlin/com/github/anivanovic/stockmine/Server.kt
+++ src/main/kotlin/com/github/anivanovic/stockmine/Server.kt
@@ -1,7 +1,6 @@
 package com.github.anivanovic.stockmine
 
 import io.ktor.application.*
-import io.ktor.auth.*
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -12,8 +11,7 @@ import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.netty.*
-import io.ktor.util.*
-import java.util.UUID
+import java.util.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
@@ -35,24 +33,25 @@ fun Application.module(testing: Boolean = false, development: Boolean = false) {
             resources("files")
         }
 
-        val ub = URLBuilder(REDDIT_AUTORIZE)
-        ub.parameters.append("client_id", clientId)
-        ub.parameters.append("response_type", "code")
-        ub.parameters.append("state", UUID.randomUUID().toString())
-        ub.parameters.append("redirect_uri", redirect_uri)
-        ub.parameters.append("duration", "permanent")
-        ub.parameters.append("scope", "read")
+
         get("/reddit-auth") {
+            val ub = URLBuilder(REDDIT_AUTORIZE)
+            ub.parameters.append("client_id", clientId)
+            ub.parameters.append("response_type", "code")
+            ub.parameters.append("state", UUID.randomUUID().toString())
+            ub.parameters.append("redirect_uri", redirect_uri)
+            ub.parameters.append("duration", "permanent")
+            ub.parameters.append("scope", "read")
             call.respondRedirect(ub.buildString(), false)
         }
         get("/reddit") {
-            val cred = UserPasswordCredential(clientId, System.getenv("REDDIT_SECRET"))
+            val cred = (clientId + ":" + System.getenv("REDDIT_SECRET")).toByteArray()
+            val auth = Base64.getEncoder().encode(cred).toString(Charsets.UTF_8)
             val res: HttpResponse = client.post(REDDIT_AUTH) {
                 parameter("grant_type", "authorization_code")
                 parameter("code", call.request.queryParameters["code"])
                 parameter("redirect_uri", redirect_uri)
-                log.info("Cred: $cred")
-                headers.append("Authorization", "Basic $cred")
+                headers.append("Authorization", "Basic $auth")
             }
             val body: String = res.receive()
             call.respondText(body, ContentType.Application.Json)
